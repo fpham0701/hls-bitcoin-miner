9d17d1b6e47fa49d0eb9efb0bbbc2b47b9b027ff
fdp25 <fdp25@cornell.edu>
2024-11-12 00:44:09 -0500

    Initialized zedboard folder
    - Added files for zedboard
    - inserted "TODO"s for future ref

---
+++ zedboard/host.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <math.h>
+#include <assert.h>
+
+#include <iostream>
+#include <fstream>
+
+#include "typedefs.h"
+#include "miner.h"
+#include "sha256.h"
+#include "util.h"
+
+//TODO Add code for host
