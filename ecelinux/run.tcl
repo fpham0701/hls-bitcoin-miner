8db0a7a67fd51d1c06562a26fc2f92b92c6cbd98
fdp25 <fdp25@cornell.edu>
2024-12-05 12:53:45 -0500

    Updated to have dut top level

--- ecelinux/run.tcl
+++ ecelinux/run.tcl
@@ -10,8 +10,8 @@ set hls_prj sha.prj
 open_project ${hls_prj} -reset
 
 # Top function of the design is "dut"
-# set_top dut
-set_top hash1
+set_top dut
+# set_top hash1
 # set_top hashblock
 # set_top mineblock
 
